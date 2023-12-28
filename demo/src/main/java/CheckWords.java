3cc08e6a1fe9e3343990e08c75e1f8836a9903ab
hyejung <hhj0988a@semi-ts.com>
2023-12-28 11:07:20 +0900

    add CheckWords

---
+++ demo/src/main/java/CheckWords.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+
+public class CheckWords{
+    public static void main(String line){
+        HashMap<String,Integer> hMap = new HashMap();
+        String lowerCased = line.toLowerCase();
+        String[] words = lowerCased.split(" ");
+
+        for(String w:words){
+            if(!hMap.containsKey((w))){
+                hMap.put(w, 1);
+            }else {
+                int num= hMap.get(w);
+                hMap.put(w, num+1);
+            }
+        }
+        
+        hMap.forEach((k,v)->System.out.println(k+" "+v));
+    }
+}
